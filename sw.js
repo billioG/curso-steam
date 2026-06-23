8d1f5d80744378f4a0b277b4c2c9516a46e919b0
billioG <59061218+billioG@users.noreply.github.com>
2026-06-23 13:29:57 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Estrategia: Cache-first para assets locales, Network-first para API
 // ============================================================
 
-const CACHE_VERSION  = 'steam-v57';
+const CACHE_VERSION  = 'steam-v63';
 const CACHE_STATIC   = `${CACHE_VERSION}-static`;
 const CACHE_DYNAMIC  = `${CACHE_VERSION}-dynamic`;
 
